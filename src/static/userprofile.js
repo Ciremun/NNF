06649b04cf7952e2ae49ced0631cf66f5c1c9632
Ciremun <Ciremun@yandex.ru>
2020-07-23 02:55:32 +0300

    use success

--- src/static/userprofile.js
+++ src/static/userprofile.js
@@ -1,7 +1,7 @@
 
 async function logout() {
     response = await postData('/logout', {SID: window.getCookie('SID')});
-    if (response.status === 200) {
+    if (response.success) {
         document.cookie = "SID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         window.location.href = `${location.protocol}//${window.location.host}`;
     } else {
