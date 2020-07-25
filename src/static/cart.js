14be65f2282627df5b00aa98ec5f76a18ba04e29
Ciremun <Ciremun@yandex.ru>
2020-07-26 00:08:34 +0300

    remove unused var

--- src/static/cart.js
+++ src/static/cart.js
@@ -3,9 +3,8 @@ async function addToCart(type) {
     if (type === 'complex') {
         let item = event.path.find(e => e.classList.contains('category_head'))
                              .getElementsByClassName('category_label')[0].innerText.split(' '),
-            response = await postData('/buy', {SID: getCookie('SID'), item: item.slice(0, -2).join(' '),
-                                            price: `${item[item.length - 2]}`,
-                                            type: type, username: window.username});
+            response = await postData('/buy', {item: item.slice(0, -2).join(' '), price: `${item[item.length - 2]}`,
+                                               type: type, username: window.username});
             if (response.success) {
                 alert('success');
             } else {
