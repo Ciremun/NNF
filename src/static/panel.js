3e870f17ca2b5b698bd27b95d373594e9041704a
Ciremun <Ciremun@yandex.ru>
2020-08-28 20:53:56 +0300

    account share init

--- src/static/panel.js
+++ src/static/panel.js
@@ -3,26 +3,35 @@ function loginEnterKey() {
     if (event.key === 'Enter') login();
 }
 
-async function login() {
-    login_response = await postData('/login', {displayname: userfield.value, password: passfield.value});
+async function processLoginResponse(login_response) {
     if (login_response.success) {
         let SID = getCookie('SID'),
             now = new Date();
         if (SID) postData('/logout', {SID: SID});
         now.setMonth(now.getMonth() + 1);
         document.cookie = `SID=${login_response.SID}; expires=${now.toUTCString()}; path=/;`;
-        window.location.pathname = '/menu';
+        window.location.reload(true);
     } else {
         showAlert(login_response.message);
-        passfield.value = "";
     }
+}
+
+async function login() {
+    login_response = await postData('/login', {displayname: userfield.value, password: passfield.value});
+    await processLoginResponse(login_response);
+    passfield.value = "";
+}
+
+async function shareLogin() {
+    login_response = await postData('/login', {target: Number(event.target.dataset.userid)});
+    await processLoginResponse(login_response);
 }
 
 async function logout() {
     let response = await postData('/logout', {SID: getCookie('SID')});
     if (response.success) {
         document.cookie = "SID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        window.location.pathname = '/';
+        window.location.reload(true);
     } else {
         window.location.reload(true);
     }
