9e124545ecca5d0494076a06676086d1b213e918
Ciremun <Ciremun@yandex.ru>
2021-02-06 22:44:42 +0300

    Add TODO(#1)

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 
 if __name__ == '__main__':
     import src.server
+
+# TODO(#1): rework adaptive
+# TODO: admin board: user roles management
+# TODO: admin board: lock orders
+# TODO: weekly menu, get menu by date
