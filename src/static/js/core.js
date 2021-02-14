8bd8b589f116c519ce9451be92592c5104272fe1
Ciremun <Ciremun@yandex.ru>
2021-02-14 20:52:50 +0300

    format

--- src/static/js/core.js
+++ src/static/js/core.js
@@ -2,18 +2,18 @@
 
 async function postData(url, data = {}) {
     const response = await fetch(url, {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST',
-      body: JSON.stringify(data)
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(data)
     });
     return response.json();
 }
 
 function showAlert(message) {
     let alerts = document.getElementsByClassName('alert');
-    if (alerts.length !== 0) 
+    if (alerts.length !== 0)
         for (item of alerts)
             item.remove();
     let outer_div = document.createElement('div'),
@@ -31,9 +31,9 @@ function between(x, min, max) {
 
 // cart
 
-async function cartAction(e, act=null, amount=null) {
+async function cartAction(e, act = null, amount = null) {
     if (act === null) act = e.value;
-    let data = {act: act};
+    let data = { act: act };
     if (amount !== null) data.amount = +amount;
     if (!['cartsbm', 'cartcl'].includes(act)) data.productID = +e.dataset.itemid;
     let response = await postData('/cart', data);
@@ -59,8 +59,8 @@ function clearTypingTimer() {
 // login
 
 let loginButton = document.getElementById("showmodal"),
-    userfield   = document.getElementById('userfield'),
-    passfield   = document.getElementById('passfield');
+    userfield = document.getElementById('userfield'),
+    passfield = document.getElementById('passfield');
 
 async function login() {
     let pair = [userfield.value, passfield.value];
@@ -70,7 +70,7 @@ async function login() {
             if (!between(code, 48, 57) && !between(code, 65, 90) && !between(code, 97, 122))
                 return showAlert('Ошибка: только английские символы и числа');
         }
-    let response = await postData('/login', {displayname: pair[0], password: pair[1]});
+    let response = await postData('/login', { displayname: pair[0], password: pair[1] });
     if (response.success) window.location.reload();
     if (response.message) showAlert(response.message);
 }
