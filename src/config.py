869e0bd15e6c9997e082b444b07760ddb48bf255
Ciremun <Ciremun@yandex.ru>
2021-02-15 22:58:46 +0300

    refactor

--- src/config.py
+++ src/config.py
@@ -1,10 +1,8 @@
-from os.path import join, dirname
-
 from dotenv import load_dotenv
+
+load_dotenv()
 
 flaskLogging = True
 https = False
 
 users_can_order = True
-
-load_dotenv(join(dirname(__name__), '.env'))
